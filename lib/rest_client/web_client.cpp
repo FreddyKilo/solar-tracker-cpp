cedbb5c018f1d0e7f25017b2ae66230827214eb2
Freddy <freddy.kilo@gmail.com>
2022-08-15 08:40:46 -0700

    Set utc offset based on response from world api

--- lib/rest_client/web_client.cpp
+++ lib/rest_client/web_client.cpp
@@ -3,12 +3,17 @@
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <tuple>
+#include <string>
 
 #include "web_client.h"
+
+using namespace std;
 
 WebClient::WebClient()
 {
     m_world_time_api_url = "http://worldtimeapi.org";
+    m_utc_offset = make_tuple(0, 0);
 }
 
 DynamicJsonDocument WebClient::ntp_get_current_time()
@@ -27,10 +32,12 @@ DynamicJsonDocument WebClient::ntp_get_current_time()
 DynamicJsonDocument WebClient::_map_result_from_ntp_client(NTPClient ntp_client)
 {
     DynamicJsonDocument result_map(1024);
-    int formatted_hour = (ntp_client.getHours() + m_utc_offset_hours + 24) % 24;
+    tuple<int, int> utc_offset = _get_utc_offset();
+    int adjusted_hour = (ntp_client.getHours() + get<0>(utc_offset) + 24) % 24;
+    int adjusted_minutes = (ntp_client.getMinutes() + get<1>(utc_offset) + 60) % 60;
 
-    result_map["hours"] = formatted_hour;
-    result_map["minites"] = ntp_client.getMinutes();
+    result_map["hours"] = adjusted_hour;
+    result_map["minites"] = adjusted_minutes;
     result_map["seconds"] = ntp_client.getSeconds();
 
     return result_map;
@@ -48,6 +55,8 @@ DynamicJsonDocument WebClient::get_time_by_ip()
     {
         String response = m_http_client.getString();
         json_response = _deserialize_response(json_response, response);
+        string utc_offset = json_response["utc_offset"];
+        _set_utc_offset(utc_offset);
     }
 
     m_http_client.end();
@@ -60,4 +69,17 @@ DynamicJsonDocument WebClient::_deserialize_response(DynamicJsonDocument doc, St
 {
     deserializeJson(doc, response);
     return doc;
+}
+
+tuple<int, int> WebClient::_get_utc_offset()
+{
+    return m_utc_offset;
+}
+
+void WebClient::_set_utc_offset(string utc_offset)
+{
+    string hours = utc_offset.substr(0, utc_offset.find(":"));
+    string minutes = utc_offset.substr(utc_offset.find(":") + 1);
+
+    m_utc_offset = make_tuple(stoi(hours), stoi(minutes));
 }
--- lib/rest_client/web_client.h
+++ lib/rest_client/web_client.h
@@ -7,16 +7,21 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
+#include <tuple>
+#include <string>
 
 class WebClient
 {
 private:
     WiFiClient m_wifi_client;
     HTTPClient m_http_client;
-    int m_utc_offset_hours;
     String m_world_time_api_url;
+    std::tuple<int, int> m_utc_offset;
+
     DynamicJsonDocument _map_result_from_ntp_client(NTPClient ntp_client);
     DynamicJsonDocument _deserialize_response(DynamicJsonDocument doc, String response);
+    std::tuple<int, int> _get_utc_offset(void);
+    void _set_utc_offset(std::string utc_offset);
 
 public:
     WebClient(void);
