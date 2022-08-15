b02cf4b55c50c7e9b5ccce7ab04535d07e7018c9
Freddy <freddy.kilo@gmail.com>
2022-08-14 20:02:23 -0700

    Add client to call world time API

--- lib/rest_client/web_client.cpp
+++ lib/rest_client/web_client.cpp
@@ -1,36 +1,63 @@
 #include <Arduino.h>
 #include <NTPClient.h>
 #include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
 #include "web_client.h"
 
 WebClient::WebClient()
 {
-    m_utc_offset_hours = -7;
+    m_world_time_api_url = "http://worldtimeapi.org";
 }
 
-DynamicJsonDocument WebClient::get_current_time()
+DynamicJsonDocument WebClient::ntp_get_current_time()
 {
-    WiFiUDP m_ntp_udp;
-    NTPClient m_time_client(m_ntp_udp, "pool.ntp.org");
+    WiFiUDP ntp_udp;
+    NTPClient ntp_client(ntp_udp, "pool.ntp.org");
 
-    m_time_client.begin();
-    m_time_client.update();
-
-    DynamicJsonDocument current_time = _map_result_from_time_client(m_time_client);
-    m_time_client.end();
+    ntp_client.begin();
+    ntp_client.update();
+    DynamicJsonDocument current_time = _map_result_from_ntp_client(ntp_client);
+    ntp_client.end();
 
     return current_time;
 }
 
-DynamicJsonDocument WebClient::_map_result_from_time_client(NTPClient time_client)
+DynamicJsonDocument WebClient::_map_result_from_ntp_client(NTPClient ntp_client)
 {
     DynamicJsonDocument result_map(1024);
-    int formatted_hour = (time_client.getHours() + m_utc_offset_hours + 24) % 24;
+    int formatted_hour = (ntp_client.getHours() + m_utc_offset_hours + 24) % 24;
 
     result_map["hours"] = formatted_hour;
-    result_map["minites"] = time_client.getMinutes();
-    result_map["seconds"] = time_client.getSeconds();
+    result_map["minites"] = ntp_client.getMinutes();
+    result_map["seconds"] = ntp_client.getSeconds();
 
     return result_map;
+}
+
+DynamicJsonDocument WebClient::get_time_by_ip()
+{
+    DynamicJsonDocument json_response(1024);
+
+    String path = "/api/ip";
+    m_http_client.begin(m_wifi_client, m_world_time_api_url + path);
+    int response_code = m_http_client.GET();
+
+    if (response_code == 200)
+    {
+        String response = m_http_client.getString();
+        json_response = _deserialize_response(json_response, response);
+    }
+
+    m_http_client.end();
+
+    json_response["code"] = response_code;
+    return json_response;
+}
+
+DynamicJsonDocument WebClient::_deserialize_response(DynamicJsonDocument doc, String response)
+{
+    deserializeJson(doc, response);
+    return doc;
 }
--- lib/rest_client/web_client.h
+++ lib/rest_client/web_client.h
@@ -4,18 +4,25 @@
 #include <Arduino.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #include <ArduinoJson.h>
 
 class WebClient
 {
 private:
+    WiFiClient m_wifi_client;
+    HTTPClient m_http_client;
     int m_utc_offset_hours;
-    DynamicJsonDocument _map_result_from_time_client(NTPClient time_client);
+    String m_world_time_api_url;
+    DynamicJsonDocument _map_result_from_ntp_client(NTPClient ntp_client);
+    DynamicJsonDocument _deserialize_response(DynamicJsonDocument doc, String response);
 
 public:
     WebClient(void);
 
-    DynamicJsonDocument get_current_time(void);
+    DynamicJsonDocument ntp_get_current_time(void);
+    DynamicJsonDocument get_time_by_ip(void);
 };
 
 #endif
