c5dd3add0a71f01b21e9b69d90c1c7f8d2f1b114
Freddy <freddy.kilo@gmail.com>
2022-08-21 07:07:51 -0700

    Add constants file

---
+++ lib/helpers/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants {
+    constexpr double DEEP_SLEEP_SECOND = 1e6;
+    
+    constexpr int DELAY_SECOND = 1000;
+    constexpr int DELAY_MINUTE = 60 * DELAY_SECOND;
+    constexpr int SECONDS_IN_DAY = 86400;
+
+    constexpr int DEFAULT_AZIMUTH_POSITION = 180;
+    constexpr int DEFAULT_ALTITUDE_POSITION = 0;
+}
+
+#endif
