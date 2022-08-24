a9241aa9b549fa28ba827198e4109da46fc93a07
Freddy <freddy.kilo@gmail.com>
2022-08-24 07:24:46 -0700

    Update panel servo to use standard arduino servo library

--- lib/panel_servo/panel_servo.cpp
+++ lib/panel_servo/panel_servo.cpp
@@ -1,17 +1,14 @@
-#include <ESP8266_ISR_Servo.h>
 #include "panel_servo.h"
 
 PanelServo::PanelServo(int pinout, int min_microseconds, int max_microseconds)
 {
+    _servo.attach(pinout, min_microseconds, max_microseconds);
     _pinout = pinout;
-    _servo_id = ISR_Servo.setupServo(pinout, min_microseconds, max_microseconds);
 }
 
-void PanelServo::set_target(int target, int speed)
+void PanelServo::set_target(int target)
 {
     if (_pinout == D5) target = map_azimuth_angle(target);
-    if (speed > 100) speed = 100;
-    if (speed < 0) speed = 0;
 
     Serial.println("setting servo on pin " + String(_pinout) + " to " + String(target) + " degrees");
 
@@ -20,19 +17,19 @@ void PanelServo::set_target(int target, int speed)
         int diff = target - _angle;
         int increment = diff / abs(diff);
 
-        ISR_Servo.setPosition(_servo_id, _angle + increment);
+        _servo.write(_angle + increment);
 
         _angle += increment;
-        delay(200 - speed * 2);
     }
 }
 
 int PanelServo::get_angle()
 {
-    return ISR_Servo.getPosition(_servo_id);
+    return _servo.read();
 }
 
 int PanelServo::map_azimuth_angle(int sun_azimuth)
 {
-  return 270 - sun_azimuth;
+    if (sun_azimuth > 270) return 0;
+    return 270 - sun_azimuth;
 }
--- lib/panel_servo/panel_servo.h
+++ lib/panel_servo/panel_servo.h
@@ -1,17 +1,20 @@
 #ifndef PANEL_SERVO
 #define PANEL_SERVO
 
+#include <Servo.h>
+
 class PanelServo
 {
 public:
     PanelServo(int pinout, int min_microseconds, int max_microseconds);
 
-    void set_target(int angle, int speed);
+    void set_target(int angle);
     int get_angle();
     void set_speed(int speed);
     int map_azimuth_angle(int sun_azimuth);
 
 private:
+    Servo _servo;
     int _pinout;
     int _servo_id;
     float _angle;
